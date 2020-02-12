c647268adb2b0e2fbf6c9de4a7be52ade169d392
Jessica Pope <jessica.pope.co@gmail.com>
2020-02-12 15:23:22 -0700

    Set Default View to HomeScreen

--- frontend/headsupcode/App.js
+++ frontend/headsupcode/App.js
@@ -1,19 +1,14 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack'
+import HomeScreen from './src/HomeScreen'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+const navigator = createStackNavigator({
+  Home:HomeScreen
+},{
+  initialRouteName: 'Home',
+  defaultNavigationOptions:{
+    title:'Home'
+  }
+})
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default createAppContainer(navigator)
