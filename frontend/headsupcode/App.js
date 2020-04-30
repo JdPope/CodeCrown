c917c29ab25767f38409ab9c70ec46eebe97a212
Nepsaco <tobie.tsuzuki@gmail.com>
2020-04-30 15:11:55 -0600

    adds styling for final screen. adds function to render list of all quesitons and highlight correct responses from card props

--- frontend/headsupcode/App.js
+++ frontend/headsupcode/App.js
@@ -2,12 +2,14 @@ import {createAppContainer} from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack'
 import HomeScreen from './src/screens/HomeScreen'
 import GameScreen from './src/screens/GameScreen'
+import FinalScreen from './src/components/FinalScreen'
 
 const navigator = createStackNavigator({
     Home: HomeScreen,
     Game: GameScreen,
+    Final: FinalScreen,
 },{
-    initialRouteName: 'Home',
+    initialRouteName: 'Final',
     defaultNavigationOptions: {
         headerShown: false,
     }
