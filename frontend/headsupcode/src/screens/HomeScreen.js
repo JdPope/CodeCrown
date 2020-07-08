1fcb7851bd3b04ee691d9cf4ff350bb0f1371c83
Tobie Tsuzuki <tobie.tsuzuki@gmail.com>
2020-07-08 11:46:28 -0600

    updates locking screen orientation on home screen

--- frontend/headsupcode/src/screens/HomeScreen.js
+++ frontend/headsupcode/src/screens/HomeScreen.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {StatusBar, Text, SafeAreaView, ScrollView, View} from 'react-native'
+import { ScreenOrientation } from 'expo'
 import Deck from '../components/Deck'
 import { styles } from '../styles/style';
 
 const HomeScreen = ({navigation}) => {
+  useEffect(() => {
+    ScreenOrientation.lockAsync(ScreenOrientation.Orientation.PORTRAIT_UP)
+  }, [])  
+
   const startGame = (deck) => navigation.navigate('Game', {deck:deck})
 
   const deckRender = navigation.state.params.decks.map(cardDeck => {
