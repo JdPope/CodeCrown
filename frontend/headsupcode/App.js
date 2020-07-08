f05d1d41683c4d6c5b3dd873d2d527e6e85a082e
Jessica Pope <jessica.pope.co@gmail.com>
2020-07-08 09:07:02 -0600

    Begin refactoring out Landing Page. Broken

--- frontend/headsupcode/App.js
+++ frontend/headsupcode/App.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator } from 'react-navigation-stack'
 import HomeScreen from './src/screens/HomeScreen'
@@ -5,13 +6,32 @@ import GameScreen from './src/screens/GameScreen'
 import FinalScreen from './src/components/FinalScreen'
 import LandingScreen from './src/screens/LandingScreen'
 
+const App  = () =>{
+const URL = 'https://headsup-api.herokuapp.com/decks'
+  const [decks, setDecks] = useState([])
+
+
+  useEffect(()=>{
+    fetch(URL)
+      .then((response) => response.json())
+      .then( decks  => {
+        setDecks(decks)
+      })
+      .catch( error => {
+        console.error(error)
+      })
+  }, [])
+}
 const navigator = createStackNavigator({
     Home: HomeScreen,
     Game: GameScreen,
     Final: FinalScreen,
     Landing:LandingScreen,
 },{
-    initialRouteName: 'Landing',
+    initialRouteName: 'Home',
+    initialRouteParams:{
+        decks:decks
+    },
     defaultNavigationOptions: {
         headerShown: false,
     }
