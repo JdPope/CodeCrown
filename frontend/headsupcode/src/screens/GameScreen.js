e2d213569c712a9181223f0770fdbe8d576bd104
Jessica Pope <jessica.pope.co@gmail.com>
2020-07-19 15:09:01 -0600

    Refactor GameScreen into Functional Component with hooks

--- frontend/headsupcode/src/screens/GameScreen.js
+++ frontend/headsupcode/src/screens/GameScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useState, useEffect} from 'react';
 import { View } from 'react-native';
 import { DeviceMotion } from 'expo-sensors';
 import { styles } from '../styles/style';
@@ -7,104 +7,106 @@ import GameCard from '../components/GameCard';
 import FinalScreen from '../components/FinalScreen';
 
 const GameScreen = (props) => {
+  
 
   const [remainingTime, setRemainingTime] = useState(63)
   const [timer, setTimer] = useState(null)
   const [cardIndex, setCardIndex] = useState(0)
   const [deviceMotionActive, setDeviceMotionActive] = useState(true)
-  const [cards, setCards] = UseState(props.navigation.state.params.deck.cards)
+  const [cards, setCards] = useState(props.navigation.state.params.deck.cards)
 
 
-  useEffect(() => {
-    checkDeviceMotion();
-    randomizeCards();
-  })
+//   useEffect(() => {
+//     checkDeviceMotion();
+//     randomizeCards();
+//   })
 
- const checkDeviceMotion = async () => {
-    await DeviceMotion.isAvailableAsync()
-      ? setDeviceMotionActive(true)
-      : setDeviceMotionActive(false)
-  }
+return <h1>GameScreen</h1>
+//  const checkDeviceMotion = async () => {
+//     await DeviceMotion.isAvailableAsync()
+//       ? setDeviceMotionActive(true)
+//       : setDeviceMotionActive(false)
+//   }
 
- const randomizeCards = () => {
-    const randomCards = cards
-      .map((card) => ({ sort: Math.random(), ...card }))
-      .sort((a, b) => a.sort - b.sort);
+//  const randomizeCards = () => {
+//     const randomCards = cards
+//       .map((card) => ({ sort: Math.random(), ...card }))
+//       .sort((a, b) => a.sort - b.sort);
 
-    setCards(randomCards)
-  }
+//     setCards(randomCards)
+//   }
 
-  const startTimer = () => {
-    setTimer(setInterval(decrementTimer, 1000));
-  }
+//   const startTimer = () => {
+//     setTimer(setInterval(decrementTimer, 1000));
+//   }
 
- const decrementTimer = () => {
-    setRemainingTime(remainingTime - 1)
-  }
+//  const decrementTimer = () => {
+//     setRemainingTime(remainingTime - 1)
+//   }
 
- const clearTimer = () => {
-    clearInterval(timer);
-    setTimer(null);
-  }
+//  const clearTimer = () => {
+//     clearInterval(timer);
+//     setTimer(null);
+//   }
 
-  const currentCard = () => {
-    return cards[cardIndex];
-  }
+//   const currentCard = () => {
+//     return cards[cardIndex];
+//   }
 
-  const nextCard = () => {  
-    if (cardIndex < (cards.length - 1)) {
-      setCardIndex( cardIndex + 1 );
-    } else {
-      setRemainingTime(0)
-    }
-  }
+//   const nextCard = () => {  
+//     if (cardIndex < (cards.length - 1)) {
+//       setCardIndex( cardIndex + 1 );
+//     } else {
+//       setRemainingTime(0)
+//     }
+//   }
 
-  const handleUserResponse = (isCorrect) => {
-    const newCardsArray = cards
-    newCardsArray[cardIndex].isCorrect = isCorrect;
-    setCards(newCardsArray);
-  }
+//   const handleUserResponse = (isCorrect) => {
+//     const newCardsArray = cards
+//     newCardsArray[cardIndex].isCorrect = isCorrect;
+//     setCards(newCardsArray);
+//   }
 
- const returnHome = (event) => props.navigation.navigate('Home')
+//  const returnHome = (event) => props.navigation.navigate('Home')
 
-  const renderComponent = () => {
-    if (remainingTime > 60) {
-      return (
-        <Countdown
-          startTimer={startTimer}
-          decrementTimer={decrementTimer}
-          remainingTime={remainingTime}
-        />
-      );
-    } if (remainingTime > 0) {
-      return (
-        <GameCard
-          remainingTime={remainingTime}
-          decrementTimer={decrementTimer}
-          clearTimer={clearTimer}
-          card={currentCard()}
-          nextCard={nextCard}
-          deviceMotionActive={deviceMotionActive}
-          handleUserResponse={handleUserResponse}
-        />
-      );
+//   const renderComponent = () => {
+//     if (remainingTime > 60) {
+//       return (
+//         <Countdown
+//           startTimer={startTimer}
+//           decrementTimer={decrementTimer}
+//           remainingTime={remainingTime}
+//         />
+//       );
+//     } if (remainingTime > 0) {
+//       return (
+//         <GameCard
+//           remainingTime={remainingTime}
+//           decrementTimer={decrementTimer}
+//           clearTimer={clearTimer}
+//           card={currentCard()}
+//           nextCard={nextCard}
+//           deviceMotionActive={deviceMotionActive}
+//           handleUserResponse={handleUserResponse}
+//         />
+//       );
 
-    }
+//     }
 
-    return (
-      <FinalScreen
-        returnHome={returnHome}
-        cards={cards}
-      />
-    );
-  }
+//     return (
+//       <FinalScreen
+//         returnHome={returnHome}
+//         cards={cards}
+//       />
+//     );
+//   }
 
-    const { background } = styles;
-    return (
-      <View style={background}>
-        {renderComponent()}
-      </View>
+//     const { background } = styles;
+//     return (
+//       <View style={background}>
+//         {renderComponent()}
+//       </View>
 
-    );
-
+//     );
 }
+export default GameScreen
